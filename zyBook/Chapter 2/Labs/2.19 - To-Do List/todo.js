0ee168c23018f1bfb8d07f28f88ce5404f376e8a
Aaron Solenberg <amsolenberg@gmail.com>
2024-11-01 13:36:48 -0400

    Started Lab 2.19

---
+++ zyBook/Chapter 2/Labs/2.19 - To-Do List/todo.js
@@ -0,0 +1,17 @@
+window.addEventListener("DOMContentLoaded", domLoaded);
+
+function domLoaded() {
+    // TODO: Complete the function
+}
+
+function addBtnClick() {
+    // TODO: Complete the function
+}
+
+function addTask(task) {
+    // TODO: Complete the function
+}
+
+function removeTask(event) {
+    // TODO: Complete the function
+}

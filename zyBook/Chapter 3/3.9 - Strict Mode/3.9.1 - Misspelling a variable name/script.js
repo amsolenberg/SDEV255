67aa7cd9bd0c65e5c55a4803fcf77d473f202bb8
Aaron Solenberg <amsolenberg@gmail.com>
2024-11-07 18:01:06 -0500

    Completed 3.9

---
+++ zyBook/Chapter 3/3.9 - Strict Mode/3.9.1 - Misspelling a variable name/script.js
@@ -0,0 +1,8 @@
+let humanLivesRemaining = 3;
+
+// Set to 1 if greater than 0
+if (humanLivesRemaining > 0) {
+   humanLivesRemaing = 1; // Typo: Remaining
+}
+
+console.log(humanLivesRemaining);
---
+++ zyBook/Chapter 3/3.9 - Strict Mode/3.9.2 - Strict Mode/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let humanLivesRemaining = 3;
+
+// Set to 1 if greater than 0
+if (humanLivesRemaining > 0) {
+   humanLivesRemaing = 1; // Typo: Remaining
+}
+
+console.log(humanLivesRemaining);

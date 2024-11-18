1829df0b7e984c8b9e9613e6d2971b12172dbccf
Aaron Solenberg <amsolenberg@gmail.com>
2024-11-18 15:53:06 -0500

    Completed 5.1

---
+++ zyBook/Chapter 5/5.01 - Getting Started with Node.js/5.1.3 - A Simple Node.js Web Server/script.js
@@ -0,0 +1,10 @@
+// server.js
+const http = require('http');
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {
+        'Content-Type': 'text/html',
+    });
+    response.write('<h1>Hello, Node.js!</h1>');
+    response.end();
+}).listen(3000);

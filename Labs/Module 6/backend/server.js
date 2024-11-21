dad13489227204fa4e5779c18d03e6392a65c697
Aaron Solenberg <amsolenberg@gmail.com>
2024-11-21 09:40:00 -0500

    M06P01 - Creating a post service

--- Labs/Module 6/backend/server.js
+++ Labs/Module 6/backend/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const cors = require('cors'); // Should not be used in production
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
 app.use(cors()); // Should not be used in production
+app.use(bodyParser.json());
 
 app.get('/messages', (req, res) => {
-   const messages = ['yes', 'each', 'messages'];
+  const messages = ['yes', 'each', 'messages'];
 
-   res.send(messages);
+  res.send(messages);
+});
+
+app.post('/messages', (req, res) => {
+  console.log(req.body);
+  res.json({});
 });
 
 app.listen(port, () => console.log('app running'));

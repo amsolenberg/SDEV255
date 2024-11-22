3d79831ca8c4d87b88850cd973ca51131457a07f
Aaron Solenberg <amsolenberg@gmail.com>
2024-11-22 08:33:57 -0500

    M7P02 - Creating a Node register service

---
+++ Labs/Module 7/frontend/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-btn text to="/">Messages</v-btn>
+      <v-btn text to="/NewMessage">New Message</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn text to="/Register">Register</v-btn>
+      <v-btn text>Login</v-btn>
+    </v-app-bar>>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  // import Messages from './components/Messages';
+  // import NewMessage from './components/NewMessage';
+
+  export default {
+    name: 'App',
+
+    components: {
+      // Messages,
+      // NewMessage,
+    },
+
+    data: () => ({
+      //
+    }),
+  };
+</script>

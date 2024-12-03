f4915f0f1d804c946cf70b56dcbbe66977651604
Aaron Solenberg <amsolenberg@gmail.com>
2024-12-03 15:28:17 -0500

    17 - Checking The Current User

--- Tutorials/Module 7/ninja-smoothies/controllers/authController.js
+++ Tutorials/Module 7/ninja-smoothies/controllers/authController.js
@@ -74,7 +74,7 @@ module.exports.login_post = async (req, res) => {
   }
 };
 
-module.exports.logout_get = async (req, res) => {
+module.exports.logout_get = (req, res) => {
   res.cookie('jwt', '', { maxAge: 1 });
   res.redirect('/');
 };

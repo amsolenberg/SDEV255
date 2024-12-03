0991bf5dfad0292e31b3e2cc34c7f3a481fda867
Aaron Solenberg <amsolenberg@gmail.com>
2024-12-03 08:35:36 -0500

    04 - User Model

--- Tutorials/Module 7/ninja-smoothies/controllers/authController.js
+++ Tutorials/Module 7/ninja-smoothies/controllers/authController.js
@@ -1,3 +1,5 @@
+const User = require('../models/User');
+
 module.exports.signup_get = (req, res) => {
   res.render('signup');
 };
@@ -6,15 +8,19 @@ module.exports.login_get = (req, res) => {
   res.render('login');
 };
 
-module.exports.signup_post = (req, res) => {
-    const { email, password } = req.body;
+module.exports.signup_post = async (req, res) => {
+  const { email, password } = req.body;
 
-    console.log(email, password);
-  
-    res.send('new signup');
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user);
+  } catch (err) {
+    console.error('Error creating new user:', err);
+    res.status(500).send('Internal Server Error');
+  }
 };
 
-module.exports.login_post = (req, res) => {
+module.exports.login_post = async (req, res) => {
   const { email, password } = req.body;
 
   console.log(email, password);

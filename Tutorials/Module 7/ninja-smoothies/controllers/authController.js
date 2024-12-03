88eff69c8d43f4531dd29a722b435681901db7b2
Aaron Solenberg <amsolenberg@gmail.com>
2024-12-03 15:05:45 -0500

    14 - Logging Users In (Part 2)

--- Tutorials/Module 7/ninja-smoothies/controllers/authController.js
+++ Tutorials/Module 7/ninja-smoothies/controllers/authController.js
@@ -6,6 +6,16 @@ const handleErrors = (err) => {
   console.log(err.message, err.code);
   let errors = { email: '', password: '' };
 
+  // incorrect email
+  if (err.message === 'Incorrect Email') {
+    errors.email = 'That email is not registered';
+  }
+
+  // incorrect
+  if (err.message === 'Incorrect Password') {
+    errors.password = 'That password is incorrect';
+  }
+
   // duplicate error code
   if (err.code === 11000) {
     errors.email = 'That email is already registered';
@@ -21,7 +31,7 @@ const handleErrors = (err) => {
   return errors;
 };
 
-const maxAge = 3 * 24 * 60 * 60;
+const maxAge = 3 * 24 * 60 * 60; // 3 days
 const createToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: maxAge
@@ -42,7 +52,7 @@ module.exports.signup_post = async (req, res) => {
   try {
     const user = await User.create({ email, password });
     const token = createToken(user._id);
-    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 }); // maxAge * 1000 = 3 days in milliseconds
     res.status(201).json({ user: user._id });
   } catch (err) {
     const errors = handleErrors(err);
@@ -55,8 +65,11 @@ module.exports.login_post = async (req, res) => {
 
   try {
     const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 }); // maxAge * 1000 = 3 days in milliseconds
     res.status(200).json({ user: user._id });
   } catch (err) {
-    res.status(400).json({});
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
   }
 };
